e279d94370cdcea6be725257cd2dbd81a4b1ebf2
ULT223 <574345456@qq.com>
2024-03-13 23:53:39 +0800

    1

--- service/service_cms/src/main/java/com/atguigu/educms/CmsApplication.java
+++ service/service_cms/src/main/java/com/atguigu/educms/CmsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.rigin;
 
 @SpringBootApplication
 @ComponentScan({"com.atguigu"})
